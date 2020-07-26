201480073b174fe73070939d3c2fb1deb6489a01
Vincent Poulain <vincent.poulain2@gmail.com>
2020-07-26 02:19:22 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import gql from 'graphql-tag'
 import { useGetWorldsQuery } from './generated/graphql'
+import { EmojiConvertor } from 'emoji-js'
 
 gql`
     query getWorlds {
@@ -11,18 +12,98 @@ gql`
     }
 `
 
+// var m = EmojiConvertor
+const emoji = new EmojiConvertor()
+
 export default () => {
-    const { data, loading } = useGetWorldsQuery({ variables: {} })
-    if (!data || loading) return <div> Loading ... </div>
+    const [showDetails, setShowDetails] = useState(false)
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center font-body bg-grey-100">
-            <div className="text-6xl font-black">
-                <div className="text-grey-900">Nous sommes Alan,</div>
-                <div className="text-alan-green">
-                    l'assurance santé qui fait simple.
+        <div className="w-full relative h-screen flex justify-center items-center font-body bg-grey-100">
+            <h1 className="text-3xl text-alan-blue font-black fixed right-0 top-0 mr-4">
+                HPFO
+            </h1>
+            <div className="fixed inset-y-0 left-0 h-screen max-w-sm w-full px-4 py-6 flex flex-col">
+                <div className="p-4 ml-4">
+                    <div className="text-grey-900 my-2 uppercase font-medium">
+                        Communities
+                    </div>
+                    <div className="flex flex-col space-y-1 ml-2 font-light text-grey-800">
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🙏</span> Sales
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">👨‍💻</span> Engineering
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🍩</span> Product
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🏆</span> Growth
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🎉</span>Ops
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">👩‍⚕️</span> Care
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">💁‍♂️</span> People
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">📒</span> Knowledge
+                        </div>
+                    </div>
+                </div>
+                <div className="p-4 ml-4">
+                    <div className="text-grey-900 my-2 uppercase font-medium">
+                        crew
+                    </div>
+                    <div className="flex flex-col space-y-1 ml-2 font-light text-grey-800">
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🙏</span> Member Success
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">👨‍💻</span> Sales Insurance
+                            Autonomy
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🍩</span> Claim Management
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🏆</span> Health Journal
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🎉</span> Lead Generation
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">👩‍⚕️</span> Growth Product
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">💁‍♂️</span> International
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">📒</span> B2B
+                        </div>
+                    </div>
+                </div>
+                <div className="p-4 ml-4">
+                    <div className="text-grey-900 my-2 uppercase font-medium">
+                        people
+                    </div>
+                    <div className="flex flex-col space-y-1 ml-2 font-light text-grey-800">
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🙏</span> Vincent Poulain
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">👨‍💻</span> Arthur Himmel
+                        </div>
+                        <div className="hover:text-alan-blue cursor-pointer hover:font-semibold transition-all duration-200">
+                            <span className="mr-2">🍩</span> JCS
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="ml-4 space-y-3 w-full max-w-xl rounded-lg bg-white p-4">
+            <div className="ml-4 space-y-6 w-full max-w-xl rounded-lg bg-white p-5">
                 <div className="flex space-x-2">
                     <div className="rounded overflow-hidden w-12 h-12 flex-shrink-0">
                         <img src="https://ca.slack-edge.com/T053BSKET-UTADDH24C-a0bdb6906fc8-48" />
@@ -47,10 +128,10 @@ export default () => {
                         </div>
                     </div>
                 </div>
-                <div className="space-y-4">
-                    <div className="mt-4 flex flex-row space-x-8 items-center">
-                        <div className="flex-shrink-0">☀️</div>
-                        <ul className="flex flex-col space-y-px list-disc">
+                <div className="space-y-4  pr-8">
+                    <div className="mt-6 flex flex-row space-x-8  items-center">
+                        <div className="flex-shrink-0 text-2xl">☀️</div>
+                        <ul className="flex flex-col space-y-2">
                             <li className="text-grey-800 font-light text-sm">
                                 Very happy to have organized my first Care team
                                 event, and that the team liked it 😊
@@ -65,74 +146,109 @@ export default () => {
                         </ul>
                     </div>
                     <div className="flex flex-row space-x-8 items-center">
-                        <div className="flex-shrink-0">🔥</div>
-                        <ul className="flex flex-col space-y-px list-disc">
-                            <li className="text-grey-800 font-light text-sm">
+                        <div className="flex-shrink-0 text-2xl">🔥</div>
+                        <ul className="flex flex-col space-y-px text-orange-900 font-normal text-sm">
+                            <li className="">
                                 Tough nights with Winston at the moment, but
                                 managing well thanks to Portugese sun
                             </li>
                         </ul>
                     </div>
                 </div>
-                <div className="h-px bg-grey-200 w-full my-4"></div>
-                <div className="space-y-6 mt-2">
-                    <div className="mt-4 flex flex-row space-x-8 items-center">
-                        <div>🎯</div>
+                <div className="h-px bg-grey-200 w-full"></div>
+                <div className="space-y-6 mt-2  pr-8">
+                    <div className="flex flex-row space-x-4 items-center">
+                        <div className="text-2xl">🎯</div>
                         <div className="space-y-3">
                             <div className="font-medium text-alan-indigo">
                                 Last week objectives
                             </div>
-                            <ul className="text-grey-800 font-light text-sm list-disc">
-                                <li> Ramp up on HTML PDFs creation </li>
-                                <li>
+                            <ul className="space-y-2 text-grey-800 font-light text-sm">
+                                <li className="flex space-x-2">
+                                    <img
+                                        src="check.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Ramp up on HTML PDFs creation
+                                </li>
+                                <li className="flex">
+                                    <img
+                                        src="check.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
                                     General Conditions: design document V0 and
                                     prepare back and forth on content design -
                                     Drive{' '}
                                 </li>
-                                <li>
+                                <li className="flex">
+                                    <img
+                                        src="check.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
                                     Particular Conditions: designed document V0
                                     - Drive{' '}
                                 </li>
-                                <li>
+                                <li className="flex">
+                                    <img
+                                        src="check.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
                                     SEPA mandate: designed document V0 - Drive
                                 </li>
-                                <li>
-                                    Insurance Card: update card design following
-                                    card number change (23 numbers instead of
-                                    10) - Issue{' '}
-                                </li>
-                                <li> Write 2 months self-review</li>
                             </ul>
                         </div>
                     </div>
-                    <div className="mt-4 flex flex-row space-x-8 items-center">
-                        <div>🚀</div>
+                    <div className="mt-4 flex flex-row space-x-4 items-center">
+                        <div className="text-2xl">🚀</div>
                         <div className="space-y-3">
                             <div className="font-medium text-alan-indigo">
                                 Next week objectives
                             </div>
-                            <ul className="text-grey-800 font-light text-sm">
-                                <li>
-                                    🇹🇩 Find a Doctor: challenge scope. Can we do
+                            <ul className="space-y-2 text-grey-800 font-light text-sm">
+                                <li className="flex">
+                                    <img
+                                        src="todo.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Find a Doctor: challenge scope. Can we do
                                     something more minimal?
                                 </li>
-                                <li>
-                                    🇹🇩 Documents: implement highlights and
-                                    comments on all documents - Drive
+                                <li className="flex">
+                                    <img
+                                        src="todo.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Documents: implement highlights and comments
+                                    on all documents - Drive
                                 </li>
-                                <li>
-                                    🇧🇪 Documents: implement V0 structure while
+                                <li className="flex">
+                                    <img
+                                        src="todo.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Documents: implement V0 structure while
                                     waiting for final docs - Trello Card
                                 </li>
-                                <li>
-                                    🇹🇩 Mobile App: design home screen - Trello
-                                    Card
+                                <li className="flex">
+                                    <img
+                                        src="todo.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Mobile App: design home screen - Trello Card
                                 </li>
-                                <li>
-                                    🇫🇷 Coverage: frame how to display a simple
-                                    presentation of our coverage - Trello Card
+                                <li className="flex">
+                                    <img
+                                        src="todo.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
+                                    Coverage: frame how to display a simple
+                                    presentation of our coverage
                                 </li>
-                                <li>
+                                <li className="flex">
+                                    <img
+                                        src="check.svg"
+                                        className="mx-2 h-4 w-4"
+                                    />
                                     (If time allows) Health partner: give a hand
                                     to @julien.chenat
                                 </li>
